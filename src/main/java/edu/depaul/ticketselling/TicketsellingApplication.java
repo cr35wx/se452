68000e637fc39d4ae03c1e098f461d119660da9f
skim217 <145881023+skim217@users.noreply.github.com>
2024-04-07 22:10:27 -0500

    Uploead our prototyope
    Yeah. it's prototype first version

---
+++ src/main/java/edu/depaul/ticketselling/TicketsellingApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.ticketselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/depaul/ticketselling/TicketsellingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.ticketselling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
