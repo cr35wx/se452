3e7a787e0c5913f328f98ede0eda7692758a39fa
William Berthouex <william.berthouex@gmail.com>
2024-04-18 18:16:33 -0500

    Update to new branch
    Update to new branch. Setup of components.

--- src/main/java/edu/depaul/ticketselling/backend/Ticket.java
+++ src/main/java/edu/depaul/ticketselling/backend/Ticket.java
@@ -1,8 +1,29 @@
 package edu.depaul.ticketselling.backend;
 
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
- * A Ticket is sold to a {@link Customer} and contains details for an {@link Event}
+ * A Ticket is sold to a {@link Customer} and contains details for an {@link Event}.
+ * <p>
+ * The {@code price} field is stored as an {@code integer} to avoid operations on decimal values. The {@code Ticket} class
+ * does not format the price in any way.
  */
+@Getter
+@Setter
+@Builder
 public class Ticket {
+    @Id
+    private long ticketId;
+    private int price;
+    private int seatNumber;
+    private Event event;
+    private LocalDate date;
+    private Venue venue;
 
 }
