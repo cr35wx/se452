e2d58bf8a2fcb07843894d049f21b4e6de4ac8da
William Berthouex <william.berthouex@gmail.com>
2024-04-17 23:16:12 -0500

    Basic implementation
    Basic implementation. Getter and setter methods, constructors, and some documentation.

---
+++ src/test/java/edu/depaul/ticketselling/backend/BackendComponentTest.java
@@ -0,0 +1,27 @@
+package edu.depaul.ticketselling.backend;
+
+import org.junit.jupiter.api.Test;
+
+public class BackendComponentTest {
+
+    @Test
+    public void UserCreation() {
+
+    }
+
+    @Test
+    public void TicketTest() {
+
+    }
+
+    @Test
+    public void VenueTest() {
+
+    }
+
+    @Test
+    public void EventTest() {
+
+    }
+
+}
