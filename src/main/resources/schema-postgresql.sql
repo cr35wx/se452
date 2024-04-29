43343b0ec4d32736301ca20105befd47d700aab5
cr35wx <rcuba@depaul.edu>
2024-04-29 15:02:27 -0500

    added purchases schema

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS accounts CASCADE;
 DROP TABLE IF EXISTS venues CASCADE;
 
 CREATE TABLE accounts (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email VARCHAR(254) UNIQUE NOT NULL,
     password varchar(256), -- should be hashed
     phone_number VARCHAR(20), -- what type?
@@ -12,7 +12,7 @@ CREATE TABLE accounts (
 );
 
 CREATE TABLE venues (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     phone_number VARCHAR(20) NOT NULL, -- what type?
     email VARCHAR(254) NOT NULL,
@@ -27,21 +27,33 @@ CREATE TABLE venues (
 
 
 CREATE TABLE events (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE, -- event name (tour name?)
     artist VARCHAR(50) NOT NULL,
     datetime TIMESTAMP NOT NULL,
-    venue_id INT NOT NULL,
+    venue_id BIGINT NOT NULL,
     foreign key (venue_id) REFERENCES venues(id)
 );
 
 
 CREATE TABLE tickets (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    event_id INT NOT NULL,
-    account_id INT, -- account id can be null when the ticket is not yet bought by anyone (?)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    event_id BIGINT NOT NULL,
+    account_id BIGINT, -- account id can be null when the ticket is not yet bought by anyone (?)
     seat_number varchar(255) NOT NULL,
     price INT NOT NULL, -- in cents
     foreign key (event_id) REFERENCES events(id),
     foreign key (account_id) REFERENCES accounts(id)
+);
+
+CREATE TABLE purchases (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    ticket_id BIGINT NOT NULL,
+    event_id BIGINT NOT NULL,
+    venue_id BIGINT NOT NULL,
+    account_id BIGINT NOT NULL,
+    foreign key (ticket_id) references tickets(id),
+    foreign key (event_id) references events(id),
+    foreign key (venue_id) references venues(id),
+    foreign key (account_id) references accounts(id)
 );
