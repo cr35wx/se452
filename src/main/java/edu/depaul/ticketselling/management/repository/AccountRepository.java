22f60c724462638e02e3a879fe4b8889c5e0c46b
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 22:16:42 -0500

    Update AccountRepository.java

--- src/main/java/edu/depaul/ticketselling/management/repository/AccountRepository.java
+++ src/main/java/edu/depaul/ticketselling/management/repository/AccountRepository.java
@@ -5,4 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByEmailAndPassword(String email, String password);
+
+    /**
+    * This feature was added for Event Reminder.
+    * src/main/java/edu/depaul/ticketselling/marketing/controller/EventChangeNotificationController.java
+    * src/main/java/edu/depaul/ticketselling/marketing/command/OrderConfirmationCommand.java
+    * @author Suhwan Kim
+    */
+    List<String> getEmailsByTicketId(Long id);
+    List<String> getEmailsByEventName(String name);
 }
