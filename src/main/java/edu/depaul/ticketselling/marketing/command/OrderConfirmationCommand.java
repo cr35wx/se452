605b37d2f5f092f1fbadd8c3542a96df4342c92e
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 22:31:43 -0500

    Update OrderConfirmationCommand.java

--- src/main/java/edu/depaul/ticketselling/marketing/command/OrderConfirmationCommand.java
+++ src/main/java/edu/depaul/ticketselling/marketing/command/OrderConfirmationCommand.java
@@ -1,8 +1,15 @@
 package edu.depaul.ticketselling.marketing.command;
 
+import edu.depaul.ticketselling.management.model.Ticket;
 import edu.depaul.ticketselling.marketing.service.Email;
 import edu.depaul.ticketselling.marketing.service.EmailService;
 
+/**
+ * [Marketing and communication]
+ * This code is the Command of Order Confirmation.
+ * 
+ * @author Suhwan Kim
+ */
 public class OrderConfirmationCommand {
     private EmailService emailService;
 
@@ -10,14 +17,24 @@ public class OrderConfirmationCommand {
         this.emailService = emailService;
     }
 
-    public void execute(String recipient, String eventName) {
+    public void execute(String recipient, Ticket ticket) {
         String subject = "Order Confirmation";
-        String body = "Thank you for purchasing tickets. Your Order : " + eventName;
+        StringBuilder bodyBuilder = new StringBuilder();
+        bodyBuilder.append("Order Confirmation\n\n");
+        bodyBuilder.append("Thank you for purchasing tickets. Your Order : \n");
+        bodyBuilder.append("Event Id: ").append(ticket.getEvent()).append("\n");
+        bodyBuilder.append("Seat Number: ").append(ticket.getSeatNumber()).append("\n");
+        bodyBuilder.append("Price: ").append(ticket.getPrice() / 100).append("\n");
+
+        String body = bodyBuilder.toString();
+        
         Email email = Email.builder()
                         .recipient(recipient)
                         .subject(subject)
                         .body(body)
                         .build();
+        
         emailService.sendEmail(email);
     }
+
 }
