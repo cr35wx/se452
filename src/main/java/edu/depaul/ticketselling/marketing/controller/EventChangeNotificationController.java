cbdd1e1618b8d18a66ea2b384512017db26a79df
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 22:32:01 -0500

    Update EventChangeNotificationController.java

--- src/main/java/edu/depaul/ticketselling/marketing/controller/EventChangeNotificationController.java
+++ src/main/java/edu/depaul/ticketselling/marketing/controller/EventChangeNotificationController.java
@@ -1,23 +1,43 @@
 package edu.depaul.ticketselling.marketing.controller;
 
-import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import edu.depaul.ticketselling.marketing.command.EventChangeNotificationCommand;
+import edu.depaul.ticketselling.management.repository.AccountRepository;
+import edu.depaul.ticketselling.management.model.Event;
+
 /**
  * [Marketing and communication]
- * This code is the skeleton of the E-mail marketing service.
- * 
- * Event cancellations / changes
+ * This code is Controller of Event cancellations / changes.
  * 
  * @author Suhwan Kim
  */
+@Component
 public class EventChangeNotificationController {
     private EventChangeNotificationCommand eventChangeNotificationCommand;
+    private final AccountRepository accountRepository;
 
-    public EventChangeNotificationController(EventChangeNotificationCommand eventChangeNotificationCommand) {
+    public EventChangeNotificationController(EventChangeNotificationCommand eventChangeNotificationCommand, 
+                                            AccountRepository accountRepository) {
         this.eventChangeNotificationCommand = eventChangeNotificationCommand;
+        this.accountRepository = accountRepository;
+    }
+    
+    public void handleEventChangeNotification(Event updatedEvent, boolean isUpdated) {
+        List<String> recipientEmails = accountRepository.getEmailsByEventName(updatedEvent.getName());
+
+        for (String recipient : recipientEmails) {
+            eventChangeNotificationCommand.execute(recipient, updatedEvent, isUpdated);
+        }
     }
 
-    public void handleEventChangeNotification(String recipient, String eventName, LocalDateTime eventDateTime) {
-        eventChangeNotificationCommand.execute(recipient, eventName, eventDateTime);
+    public void handleEventDeletionNotification(Event deletedEvent) {
+        List<String> recipientEmails = accountRepository.getEmailsByEventName(deletedEvent.getName());
+    
+        for (String recipient : recipientEmails) {
+            eventChangeNotificationCommand.execute(recipient, deletedEvent, false);
+        }
     }
 }
