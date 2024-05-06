486eb9dfb0b98f5ef51b7ab95af1e0d56a60d2c7
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 22:31:12 -0500

    Update EventChangeNotificationCommand.java

--- src/main/java/edu/depaul/ticketselling/marketing/command/EventChangeNotificationCommand.java
+++ src/main/java/edu/depaul/ticketselling/marketing/command/EventChangeNotificationCommand.java
@@ -1,15 +1,12 @@
 package edu.depaul.ticketselling.marketing.command;
 
-import java.time.LocalDateTime;
-
+import edu.depaul.ticketselling.management.model.Event;
 import edu.depaul.ticketselling.marketing.service.Email;
 import edu.depaul.ticketselling.marketing.service.EmailService;
 
 /**
  * [Marketing and communication]
- * This code is the skeleton of the E-mail marketing service.
- * 
- * Event cancellations / changes
+ * This code is Event cancellations / changes.
  * 
  * @author Suhwan Kim
  */
@@ -21,14 +18,47 @@ public class EventChangeNotificationCommand {
         this.emailService = emailService;
     }
 
-    public void execute(String recipient, String eventName, LocalDateTime newEventDateTime) {
+    public void execute(String recipient, Event event, boolean isUpdated) {
+        if (isUpdated) {
+            sendEventUpdateNotification(recipient, event);
+        } else {
+            sendEventDeletionNotification(recipient, event);
+        }
+    }
+
+    private void sendEventUpdateNotification(String recipient, Event event) {
         String subject = "Event Change Notification";
-        String body = "Please be informed that there has been a change in the schedule for " + eventName + ". The new date and time is " + newEventDateTime.toString();
+        StringBuilder bodyBuilder = new StringBuilder();
+        bodyBuilder.append("Your Event is Updated!:\n");
+        bodyBuilder.append("Please be informed that there has been an update in the schedule for ").append(event.getName()).append(".\n\n");
+        bodyBuilder.append("Updated Event Information:\n");
+        bodyBuilder.append("Event Name: ").append(event.getName()).append("\n");
+        bodyBuilder.append("Artist: ").append(event.getArtist()).append("\n");
+        bodyBuilder.append("Date and Time: ").append(event.getDatetime()).append("\n");
+        bodyBuilder.append("Venue: ").append(event.getVenue().getName()).append("\n");
+        bodyBuilder.append("Address: ").append(event.getVenue().getAddressLine1()).append(", ")
+                .append(event.getVenue().getAddressLine2()).append(", ").append(event.getVenue().getCity()).append(", ")
+                .append(event.getVenue().getState()).append(", ").append(event.getVenue().getPostalCode()).append("\n");
+    
+        String body = bodyBuilder.toString();
+    
         Email email = Email.builder()
-                        .recipient(recipient)
-                        .subject(subject)
-                        .body(body)
-                        .build();
+                .recipient(recipient)
+                .subject(subject)
+                .body(body)
+                .build();
+    
+        emailService.sendEmail(email);
+    }
+
+    private void sendEventDeletionNotification(String recipient, Event event) {
+        String subject = "Event Deletion Notification";
+        String body = "Please be informed that the event " + event.getName() + " has been deleted.";
+        Email email = Email.builder()
+                .recipient(recipient)
+                .subject(subject)
+                .body(body)
+                .build();
         emailService.sendEmail(email);
     }
 }
