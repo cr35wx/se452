7f6064c22a1bdb161f7cf0700e6326f2d1729049
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 21:40:06 -0500

    Update IUserRepository.java

--- src/main/java/edu/depaul/ticketselling/backend/IUserRepository.java
+++ src/main/java/edu/depaul/ticketselling/backend/IUserRepository.java
@@ -8,4 +8,10 @@ public interface IUserRepository<T extends User> extends CrudRepository<T, Long>
     User findUserByPhone(String phoneNumber);
     User findUserById(Long id);
     User findByEmailAndPassword(String email, String password);
+    /**
+    * This feature was added for Event Reminder.
+    * src/main/java/edu/depaul/ticketselling/marketing/controller/EventReminderController.java
+    * @author Suhwan Kim
+    */
+    List<User> findUsersByEvent(Event event);
 }
