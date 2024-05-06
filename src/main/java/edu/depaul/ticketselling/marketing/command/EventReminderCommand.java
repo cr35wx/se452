571796ca0a2e2b1bd1caa5d796a06bb7b68d1e48
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-06 00:59:30 -0500

    Update EventReminderCommand.java
    Change to use HTML

--- src/main/java/edu/depaul/ticketselling/marketing/command/EventReminderCommand.java
+++ src/main/java/edu/depaul/ticketselling/marketing/command/EventReminderCommand.java
@@ -1,13 +1,13 @@
 package edu.depaul.ticketselling.marketing.command;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import edu.depaul.ticketselling.backend.Event;
 import edu.depaul.ticketselling.backend.User;
 import edu.depaul.ticketselling.marketing.service.Email;
 import edu.depaul.ticketselling.marketing.service.EmailService;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/email")
@@ -28,7 +28,14 @@ public class EventReminderCommand {
     public void execute(User user, Event event) {
         String recipient = user.getEmailAddress();
         String subject = "Upcoming Event Reminder";
-        String body = "Hello,\n\nThis is a reminder that the event '" + event.getEventName() + "' will take place tomorrow. Don't forget to attend!\n\nRegards,\nThe TicketSelling Team";
+        StringBuilder bodyBuilder = new StringBuilder();
+        bodyBuilder.append("<html><body>");
+        bodyBuilder.append("<p>Hello,</p><br>");
+        bodyBuilder.append("<p>This is a reminder that the event '").append(event.getEventName()).append("' will take place tomorrow. Don't forget to attend!</p><br>");
+        bodyBuilder.append("<p>Regards,<br>The TicketSelling Team</p>");
+    
+        bodyBuilder.append("</body></html>");
+        String body = bodyBuilder.toString();
         
         Email email = Email.builder()
                         .recipient(recipient)
