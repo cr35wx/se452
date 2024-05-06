6c0bdb458d5b4d8ebd1f869feba47e703fd387b4
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-05 21:46:16 -0500

    Update EventReminderCommand.java

--- src/main/java/edu/depaul/ticketselling/marketing/command/EventReminderCommand.java
+++ src/main/java/edu/depaul/ticketselling/marketing/command/EventReminderCommand.java
@@ -1,34 +1,34 @@
 package edu.depaul.ticketselling.marketing.command;
 
-import java.time.LocalDateTime;
-
+import edu.depaul.ticketselling.backend.Event;
+import edu.depaul.ticketselling.backend.User;
 import edu.depaul.ticketselling.marketing.service.Email;
 import edu.depaul.ticketselling.marketing.service.EmailService;
 
 /**
  * [Marketing and communication]
- * This code is the skeleton of the E-mail marketing service.
- * 
- * Event reminders
+ * This code is Event reminders of the E-mail marketing service.
  * 
  * @author Suhwan Kim
  */
-
 public class EventReminderCommand {
-    private EmailService emailService;
+    private final EmailService emailService;
 
     public EventReminderCommand(EmailService emailService) {
         this.emailService = emailService;
     }
 
-    public void execute(String recipient, String eventName, LocalDateTime eventDateTime) {
-        String subject = "Event Reminder";
-        String body = "Reminder: You have an upcoming event " + eventName + " on " + eventDateTime.toString();
+    public void execute(User user, Event event) {
+        String recipient = user.getEmailAddress();
+        String subject = "Upcoming Event Reminder";
+        String body = "Hello,\n\nThis is a reminder that the event '" + event.getEventName() + "' will take place tomorrow. Don't forget to attend!\n\nRegards,\nThe TicketSelling Team";
+        
         Email email = Email.builder()
                         .recipient(recipient)
                         .subject(subject)
                         .body(body)
                         .build();
+        
         emailService.sendEmail(email);
     }
 }
