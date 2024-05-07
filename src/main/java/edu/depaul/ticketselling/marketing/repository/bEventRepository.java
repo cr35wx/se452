71c0139468b53fcab047462045d890652b6f05d6
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-06 19:04:25 -0500

    Delete src/main/java/edu/depaul/ticketselling/marketing/repository directory

--- src/main/java/edu/depaul/ticketselling/marketing/repository/bEventRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.depaul.ticketselling.marketing.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import edu.depaul.ticketselling.marketing.model.bEvent;
-
-public interface bEventRepository extends CrudRepository<bEvent, Long> {
-    bEvent findByEventName(String eventName);
-}
