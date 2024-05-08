6cabb08997c909285810183555d41760b99d627f
cr35wx <rcuba@depaul.edu>
2024-05-07 21:09:03 -0500

    mapped models to schema, again.

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -5,35 +5,35 @@ DROP TABLE IF EXISTS venues CASCADE;
 DROP TABLE IF EXISTS purchases CASCADE;
 
 CREATE TABLE accounts (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR(254) UNIQUE NOT NULL,
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email_address VARCHAR(254) UNIQUE NOT NULL,
     password varchar(256), -- should be hashed
     phone_number VARCHAR(20), -- what type?
-    created_date DATE NOT NULL
+    creation_date DATE NOT NULL
 );
 
 CREATE TABLE venues (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL, -- what type?
-    email VARCHAR(254) NOT NULL,
-    capacity INT NOT NULL,
-    address_line1 VARCHAR(100) NOT NULL,
-    address_line2 VARCHAR(100),
-    city VARCHAR(50) NOT NULL,
+    venue_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    venue_name VARCHAR(50) NOT NULL,
+--     phone_number VARCHAR(20) NOT NULL, -- what type?
+--     email VARCHAR(254) NOT NULL,
+    seating_capacity INT NOT NULL,
+    address_line1 VARCHAR(127) NOT NULL,
+    address_line2 VARCHAR(127),
+    city VARCHAR(63) NOT NULL,
     state CHAR(2) NOT NULL,
-    postal_code VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(16) NOT NULL,
     description TEXT
 );
 
 
 CREATE TABLE events (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE, -- event name (tour name?)
+    event_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    event_name VARCHAR(50) NOT NULL UNIQUE, -- event name (tour name?)
     artist VARCHAR(50) NOT NULL,
-    datetime TIMESTAMP NOT NULL,
+    date_time TIMESTAMP NOT NULL,
     venue_id BIGINT NOT NULL,
-    foreign key (venue_id) REFERENCES venues(id)
+    foreign key (venue_id) REFERENCES venues(venue_id)
 );
 
 
@@ -41,10 +41,10 @@ CREATE TABLE tickets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     event_id BIGINT NOT NULL,
     account_id BIGINT, -- account id can be null when the ticket is not yet bought by anyone (?)
-    seat_number varchar(255) NOT NULL,
+    seat_number INT NOT NULL,
     price INT NOT NULL, -- in cents
-    foreign key (event_id) REFERENCES events(id),
-    foreign key (account_id) REFERENCES accounts(id)
+    foreign key (event_id) REFERENCES events(event_id),
+    foreign key (account_id) REFERENCES accounts(user_id)
 );
 
 CREATE TABLE purchases (
@@ -54,7 +54,7 @@ CREATE TABLE purchases (
     venue_id BIGINT NOT NULL,
     account_id BIGINT NOT NULL,
     foreign key (ticket_id) references tickets(id),
-    foreign key (event_id) references events(id),
-    foreign key (venue_id) references venues(id),
-    foreign key (account_id) references accounts(id)
+    foreign key (event_id) references events(event_id),
+    foreign key (venue_id) references venues(venue_id),
+    foreign key (account_id) references accounts(user_id)
 );
