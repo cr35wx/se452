8374b21f5f6544a067cbd676d335bcd6b89f24a3
skim217 <145881023+skim217@users.noreply.github.com>
2024-05-07 20:40:08 -0500

    Update and rename OrderConfirmationController.txt to OrderConfirmationController.java

--- src/main/java/edu/depaul/ticketselling/marketing/controller/OrderConfirmationController.java
+++ src/main/java/edu/depaul/ticketselling/marketing/controller/OrderConfirmationController.java
@@ -1,6 +1,10 @@
 package edu.depaul.ticketselling.marketing.controller;
 
-import edu.depaul.ticketselling.marketing.model.*;
+import edu.depaul.ticketselling.backend.Purchase;
+
+import edu.depaul.ticketselling.marketing.command.OrderConfirmationCommand;
+
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import edu.depaul.ticketselling.marketing.command.OrderConfirmationCommand;
 
 /**
  * [Marketing and communication]
@@ -22,8 +24,6 @@ import edu.depaul.ticketselling.marketing.command.OrderConfirmationCommand;
  * Upon receiving a request to confirm an order, it retrieves the necessary information from the request body,
  * including the purchased ticket details. It then sends confirmation emails to the purchasers by invoking
  * the execute method of the OrderConfirmationCommand.
- * 
- * This function is closely related to the model and repository of management, and may be modified if the management package is modified.
  * 
  * @author Suhwan Kim
  */
@@ -39,8 +39,27 @@ public class OrderConfirmationController {
     }
 
     @PostMapping("/confirm-order")
-    public ResponseEntity<String> confirmOrder(@RequestBody mkTicket purchasedTicket) {
-        orderConfirmationCommand.execute(purchasedTicket);
+    public ResponseEntity<String> confirmOrder(@RequestBody Purchase purchasedTicket) {
+        sendOrderConfirmationEmail(purchasedTicket);
         return ResponseEntity.ok("Order confirmation email sent successfully!");
+    }
+
+    private void sendOrderConfirmationEmail(Purchase purchase) {
+        String recipient = purchase.getAccount().getEmailAddress();
+
+        // Event Data
+        String eventName = purchase.getEvent().getEventName();
+        String artistName = purchase.getEvent().getArtist();
+        LocalDateTime dateTime = purchase.getEvent().getDateTime();
+
+        // Venue Data
+        String venueName = purchase.getVenue().getVenueName();
+        String venueAddress = purchase.getVenue().getAddress();
+
+        // Ticket Data
+        int seatNumber = purchase.getTicket().getSeatNumber();
+        int price = purchase.getTicket().getPrice();
+
+        orderConfirmationCommand.execute(recipient, eventName, artistName, dateTime, venueName, venueAddress, seatNumber, price);
     }
 }
